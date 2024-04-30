7097f6a65a20a67c928e60169747ba636724c97e
Joao Otavio Gallo <joao.otavio.gallo@gmail.com>
2024-04-30 17:20:51 -0300

    Voting show votes and percentage rules

--- web/modules/custom/voting/src/Helper/VotesHelper.php
+++ web/modules/custom/voting/src/Helper/VotesHelper.php
@@ -2,11 +2,13 @@
 
 namespace Drupal\voting\Helper;
 
-trait VotesHelper {
+trait VotesHelper 
+{
   /**
    * Helper function to check if user has voted on a given node.
    */
-  public function checkIfVoted($nid, $uid = NULL) {
+  public function checkIfVoted($nid, $uid = NULL) 
+  {
     $uid = empty($uid) ? \Drupal::currentUser()->id() : $uid;
     $rows = $this->select([
       ['v.uid', $uid, '='],
@@ -14,4 +16,30 @@ trait VotesHelper {
     ]);
     return !empty($rows);
   }
+  /**
+   * Get all votes for a given node.
+   * Format for percentage and total count.
+   */
+  public function getTotalVotes($nid) 
+  {
+    $rows = $this->select([['v.nid', $nid, '=']]);
+    $count = count($rows);
+    $votes = [];
+    array_walk($rows, function($row) use(&$votes) {
+      $votes[$row->pid]['count']++; 
+    });
+    foreach($votes as &$vote) {
+      $vote['percentage'] = round(($vote['count'] * 100) / $count);
+    }
+    return $votes;
+  }
+  /**
+   * @TODO
+   */
+  public function getTotalCount($nid) 
+  {
+    $rows = $this->select([['v.nid', $nid, '=']]);
+    return count($rows);
+  }
+
 }
